c5cb1854861e4be0429fd2fff35e661642719c14
PatrickQ96 <pquinn778@hotmail.co.uk>
2019-06-29 15:44:53 +0100

    adding Patrick's max scene

---
+++ ClubExpanse-Scene/Assets/Scripts/Music/clubExpanseMusic.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class clubExpanseMusic : MonoBehaviour {
+	int onSongNo = 0;
+	int numOfSongs = 0;
+
+	Text urlInputField;
+	string urlInput;
+	string tempLastSong;
+	string[] songURL;
+	bool paused = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void pausePlay(){
+		if (!paused) {
+			paused = true;
+			//player.Pause();
+		}else{
+			paused = false;
+			//player.Play();
+		}
+	}
+
+	public void playNextSong(){
+		//player.URL = songURL[1];
+		//tempLastSong = song[1];
+		//player.Play();
+		//remove first in songURLList;
+	}
+
+	public void addToQueue(){
+		numOfSongs++;
+		urlInput =	urlInputField.ToString ();
+		urlInputField.text = "";
+		//add urlInput to last in list;
+	}
+}
